aa29d978b1bb79bd7a302fb400c548787e2d3407
unknown <khoanguyenemail27032001@gmail.com>
2025-07-26 22:24:02 +0700

    Implement Hero Sroll  Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Navbar/>
+       <Hero/>
+        <div className="h-dvh bg-black"/>
+    </main>
   );
 };
 
